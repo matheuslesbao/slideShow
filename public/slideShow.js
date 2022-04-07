04298327545cdf3186d4e7701a8a381f5305510e
matheuslesbao <matheuss.lesbao@gmail.com>
2022-04-07 18:59:05 -0300

    Criação da pasta img, e alteraçoes no html e js

---
+++ public/slideShow.js
@@ -0,0 +1,18 @@
+'use strict';
+// Imagens com suas id's
+const images = [
+    { 'id': '1', 'url': './img/SenseLife01.jpg'},
+    { 'id': '2', 'url': 'Imagem aqui'},
+    { 'id': '3', 'url': 'Imagem aqui'},
+    { 'id': '4', 'url': 'Imagem aqui'},
+    { 'id': '5', 'url': 'Imagem aqui'},
+    { 'id': '6', 'url': 'Imagem aqui'},
+]
+
+const container = document.querySelector('.container-items')
+// arrow function que recebe imagem e o container
+const loadImagens = (images, container) => {
+
+}
+
+loadImagens( images, container )
